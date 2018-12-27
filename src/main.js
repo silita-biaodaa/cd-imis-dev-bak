365c8b5b135d5dde2061f0862e9351cd1483a012
lidong <642540749@qq.com>
2018-12-27 22:06:12 +0800

    测试的 #122715

--- src/main.js
+++ src/main.js
@@ -50,7 +50,7 @@ router.beforeEach((to, from, next) => {
       alert(res.data.openid + '' + res.data.nickname)
        if ( res.code == 1 ) {
          localStorage.setItem('Authorization', res.data.token) 
-          if (res.data.isFirst) {
+          if (!res.data.isFirst) {
                return  this.$router.push({
                           path: '/index' // 到登录页重新获取token
                         })
