e99f5414fcad4c7c63736e5b5aa2b6454c2b9427
lidong <642540749@qq.com>
2018-12-27 20:15:42 +0800

    测试的 #12273

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ axios.defaults.baseURL = baseURL
 
 axios.interceptors.request.use(function (config) {
   // 将token给到一个前后台约定好的key中，作为请求发送
-  let token = localStorage.getItem('Authorization')
+  let token = localStorage.getItem('Authorization') ? localStorage.getItem('Authorization') : ''
 
   // if (token) {
     config.headers['Authorization'] = token
