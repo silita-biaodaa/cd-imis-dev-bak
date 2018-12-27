b6fb58b2ddbdc0eaf77932ae1e621c0403507271
lidong <642540749@qq.com>
2018-12-27 20:51:47 +0800

    测试的 #12277

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 
 import Vue from 'vue'
 import App from './App'
-import VueRouter from 'vue-router'
+// import VueRouter from 'vue-router'
 import router from './router/router'
 // import router from 'vue-router'
-Vue.use(VueRouter);
+// Vue.use(VueRouter);
 import axios from './axios/index'
 Vue.prototype.$axios = axios
 
@@ -33,21 +33,24 @@ Vue.component('x-button', XButton)
 //   routes // (缩写) 相当于 routes: routes
 // })
 
-import { queryList } from "./api/index"
+// import { queryList } from "./api/index"
 import util from "./util/util"
 router.beforeEach((to, from, next) => {
   let code = util.getCode('code')
+  console.log(555)
   alert(code)
   if (!code) {
     util.weixinauth()
+    // console.log(1111)
+    console.log(code, 44)
     // next()
   } else {
     queryList({ code: code }).then(res => {
       console.log(res, 36)
       alert(res.data.openid + '' + res.data.nickname)
-      //  if( res == 1 ) {
-      //    localStorage.setItem('Authorization', res.data.token) 
-      //  }
+       if ( res == 1 ) {
+         localStorage.setItem('Authorization', res.data.token) 
+       }
     })
 
     next()
--- src/router/router.js
+++ src/router/router.js
@@ -1,21 +1,21 @@
 
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/page/text'
+// import index from '@/page/text'
 import Home from '@/page/index'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   redirect: '/home',
-    // },
-    // {
-    //   path: '/home',
-    //   name: 'home',
-    //   component: Home
-    // },
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    }
     // {
     //   path: '/index',
     //   name: 'index',
