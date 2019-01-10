64831a3ac91bd39e8e2a56711666ea2e67cfc261
zhangxiahui <281055769@qq.com>
2019-01-10 15:34:00 +0800

    #崇德信息平台 群组成员问题

--- src/page/groupUser.vue
+++ src/page/groupUser.vue
@@ -1,30 +1,30 @@
 
 <template>
   <div class="groupUser">
-      <v-head :head-txt="grupName"></v-head>
-       <van-search placeholder="请输入搜索关键词" v-model="keywords" @blur="ajax" />
-      <ul class="listBox" v-show=" this.list.length" >
-        <li v-for="(o,i) of list" :key="i" >
-          <div class="left">
-            <img :src="o.imgUrl">
-          </div>
-          <div class="right"  @click="jumpFriend(i)">
-            <h5>{{o.name}}</h5>
-            <p>
-              打卡第<span style="color: #E62129">{{o.pushDays}}</span>天，本月缺卡<span style="color:#0BA61D">{{o.monthLostCount}}</span>次，共缺卡<span style="color:#D8B305">{{o.lostCount}}</span>次。
-            </p>
-          </div>
-          <span class="delete" @click="deleteFn(i)" v-if="o.isCreate!=1&&type==true"></span>
-          <span class="turn" @click="turnFn(i)" v-if="o.isCreate!=1&&type==true"></span>
-        </li>
-      </ul>
-      <div class="hint" v-show=" !this.list.length && !this.keywords.trim()">
-          暂无群成员
-      </div>
-      <div class="hint" v-show=" !this.list.length && this.keywords.trim()">
-          查询无结果
-      </div>
-      <v-popup :popupShow="mask" :popupType="'tip1'" :tip-text="tipTxt" @sure="maskFn"></v-popup>
+    <v-head :head-txt="groupName"></v-head>
+    <van-search placeholder="请输入搜索关键词" v-model="keywords" @blur="ajax" />
+    <ul class="listBox" v-show=" this.list.length" >
+      <li v-for="(o,i) of list" :key="i" >
+        <div class="left">
+          <img :src="o.imgUrl">
+        </div>
+        <div class="right"  @click="jumpFriend(i)">
+          <h5>{{o.name}}</h5>
+          <p>
+            打卡第<span style="color: #E62129">{{o.pushDays}}</span>天，本月缺卡<span style="color:#0BA61D">{{o.monthLostCount}}</span>次，共缺卡<span style="color:#D8B305">{{o.lostCount}}</span>次。
+          </p>
+        </div>
+        <span class="delete" @click="deleteFn(i)" v-if="o.isCreate!=1&&type==true"></span>
+        <span class="turn" @click="turnFn(i)" v-if="o.isCreate!=1&&type==true"></span>
+      </li>
+    </ul>
+    <div class="hint" v-show=" !this.list.length && !this.keywords.trim()">
+      暂无群成员
+    </div>
+    <div class="hint" v-show=" !this.list.length && this.keywords.trim()">
+      查询无结果
+    </div>
+    <v-popup :popupShow="mask" :popupType="'tip1'" :tip-text="tipTxt" @sure="maskFn"></v-popup>
   </div>
 </template>
 <script>
@@ -107,7 +107,6 @@
         CardRecord.groupPerson(data).then(res =>{
           this.hideLoading();
           if(res){
-            that.list=res.data.list
             if(that.pageList.pageNo>1){
               that.list = that.list.concat(res.data.list);
               that.pageList.total = res.data.total;
@@ -146,6 +145,7 @@
           return false
         }
         if(scrollBottom<h){
+          this.loading();
           this.isScroll=false;
           this.pageList.pageNo+=1;
           this.ajax();
@@ -176,91 +176,92 @@
   }
 </script>
 <style lang='less' >
-.title{
-  padding: 33px;
-  background: #fff;
-  text-align: center;
-  font-size: 32px;
-  color: #010101;
-  display:flex;
-  align-items:center;
-  justify-content: space-between;
+  .title{
+    padding: 33px;
+    background: #fff;
+    text-align: center;
+    font-size: 32px;
+    color: #010101;
+    display:flex;
+    align-items:center;
+    justify-content: space-between;
   .nameTxt{
     max-width: 80%;
     overflow:hidden;
     text-overflow:ellipsis;
     white-space:nowrap
   }
-}
-.serBox{
-  background: #F5F5F5;
-  padding: 20px 32px;
+  }
+  .serBox{
+    background: #F5F5F5;
+    padding: 20px 32px;
   div{
     background: #fff;
     height: 72px;
     border-radius:10px;
   }
-}
-.listBox{
-  padding: 0 36px;
+  }
+  .listBox{
+    background: #fff;
+    padding: 0 36px;
   li{
     padding: 28px 0;
     border-bottom: 1px #f2f2f2 solid;
     display: flex;
     justify-content: space-between;
     position: relative;
-    .delete{
-      width: 31px;
-      height: 31px;
-      background: url("../assets/img/sc.png") no-repeat;
-      background-size: cover;
-      position: absolute;
-      top: 28px;
-      right: 0;
-      z-index: 999;
-    }
-    .turn{
-      width: 32px;
-      height: 32px;
-      background: url("../assets/img/zr.png") no-repeat;
-      background-size: cover;
-      position: absolute;
-      top: 28px;
-      right: 85px;
-      z-index: 999;
-    }
-    .left{
-      width: 120px;
-      img{
-        width: 100%;
-        border-radius: 50%;
-      }
-    }
-    .right{
-      width: calc(100% - 150px);
+  .delete{
+    width: 31px;
+    height: 31px;
+    background: url("../assets/img/sc.png") no-repeat;
+    background-size: cover;
+    position: absolute;
+    top: 28px;
+    right: 0;
+    z-index: 999;
+  }
+  .turn{
+    width: 32px;
+    height: 32px;
+    background: url("../assets/img/zr.png") no-repeat;
+    background-size: cover;
+    position: absolute;
+    top: 28px;
+    right: 85px;
+    z-index: 999;
+  }
+  .left{
+    width: 120px;
+  img{
+    width: 100%;
+    border-radius: 50%;
+  }
+  }
+  .right{
+    width: calc(100% - 150px);
 
 
-      h5{
-        color: #000;
-        font-size: 36px;
-        font-weight: bold;
-        margin-bottom: 40px;
-      }
-      p{
-        font-size: 24px;
-        color: #999;
-        span{
-          font-size: 28px;
-        }
-      }
-    }
+  h5{
+    color: #000;
+    font-size: 36px;
+    font-weight: bold;
+    margin-bottom: 40px;
+  }
+  p{
+    font-size: 24px;
+    color: #999;
+  span{
+    font-size: 28px;
+  }
+  }
+  }
   }
   li:last-child{
     border-bottom: none;
   }
 
-}
-.groupUser {
+  }
+  .groupUser {
   .van-search {
     padding: 20px 36px;
   }
@@ -279,11 +280,11 @@
     font-size: 40px !important;
   }
   .hint {
-     margin-top: 200px;
-     color: #666;
-     text-align: center;
-     font-size: 40px;
+    margin-top: 200px;
+    color: #666;
+    text-align: center;
+    font-size: 40px;
   }
-}
+  }
 
 </style>
